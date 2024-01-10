051b0b6a37894a715e8918292797d21a01e42da5
Eda Kalaycioglu <edakalaycioglu@gmail.com>
2024-01-10 17:53:00 +0300

    Person and Wall classes written

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,73 @@
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Person person = new Person("John", "Doe", 20, "edakalaycioglu@gmail.com", true);
+        System.out.println(" Firstname: " + person.getFirstName());
+        System.out.println("LastName: " + person.getLastName());
+        System.out.println("Age: " + person.getAge());
+        System.out.println("isTeen: " + person.isTeen());
+        System.out.println("Email: " + person.getEmail());
+        System.out.println("isStudent: " + person.isStudent());
+
+        System.out.println("--------------");
+        Wall wall = new Wall(5,4);
+
+        System.out.println("area= " + wall.getArea());
+
+        wall.setHeight(-1.5);
+
+        System.out.println("width= " + wall.getWidth());
+
+        System.out.println("height= " + wall.getHeight());
+
+        System.out.println("area= " + wall.getArea());
+
+    }
+
+    static class Person {
+        private String firstname;
+        private String lastName;
+        private int age;
+        private String email;
+
+        private boolean isStudent;
+
+        //constructor
+        public Person(String firstname, String lastName, int age, String email, boolean isStudent ) {
+            this.firstname = firstname;
+            this.lastName = lastName;
+            this.age = age;
+            this.email = email;
+            this.isStudent = isStudent;
+        }
+
+        //constructor2
+        public Person(String firstname, String lastName, int age, String address, String email, boolean isStudent) {
+            this(firstname, lastName, age,email, isStudent);
+
+        }
+
+        public String getFirstName() {
+            return this.firstname;
+        }
+
+        public String getLastName() {
+            return this.lastName;
+        }
+        public String getEmail() {
+            return this.email;
+        }
+
+
+        public int getAge() {
+            return this.age;
+        }
+
+        public boolean isTeen() {
+            return this.age >= 13 && this.age <= 19;
+        }
+        public boolean isStudent() {
+            return this.isStudent;
+        }
     }
 }
